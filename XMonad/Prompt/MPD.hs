41844d810301674470e84a3d0b6e4d022a72e95e
Daniel Schoepe <daniel.schoepe@googlemail.com>
2010-10-05 16:13:42 -0700

    Made X.P.MPD compatible with new libmpd and added a few upper version boundaries
    Ignore-this: 7594ee77b02853e1b3c30cdc360ea698
    
    darcs-hash:20101005231342-723a1-d9a21a14be02e8d5b5feb2a30d9940a8b58a142a

--- XMonad/Prompt/MPD.hs
+++ XMonad/Prompt/MPD.hs
@@ -1,21 +1,20 @@
-{-# LANGUAGE PatternGuards, Rank2Types #-}
+{-# LANGUAGE Rank2Types #-}
 
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  XMonad.Prompt.MPD
--- Copyright   :  Daniel Schoepe <daniel.schoepe@gmail.com>
+-- Copyright   :  Daniel Schoepe <daniel.schoepe@googlemail.com>
 -- License     :  BSD3-style (see LICENSE)
 --
--- Maintainer  :  Daniel Schoepe <daniel.schoepe@gmail.com>
+-- Maintainer  :  Daniel Schoepe <daniel.schoepe@googlemail.com>
 -- Stability   :  unstable
 -- Portability :  unportable
 --
--- This module lets you select songs and add/play songs with MPD by filtering
--- them by user-supplied criteria(E.g. ask for an artist, then for the album..)
+-- This module lets the user select songs and have MPD add/play them by
+-- filtering them by user-supplied criteria(E.g. ask for an artist, then for
+-- the album..)
 --
 -----------------------------------------------------------------------------
-
--- TODO: Add other backends besides mpd(e.g. amarok via dcop).
 
 module XMonad.Prompt.MPD (-- * Usage
                           -- $usage
@@ -26,15 +25,14 @@ module XMonad.Prompt.MPD (-- * Usage
                          ,findOrAdd
                          )  where
 import Control.Monad
-import Control.Applicative
 import Data.Char
+import qualified Data.Map as M
+import Data.Maybe
 import Network.MPD
-import System.IO
 import XMonad
 import XMonad.Prompt
 import XMonad.Prompt.Input
 import Data.List (nub,isPrefixOf,findIndex)
-
 
 -- $usage
 --
@@ -43,52 +41,64 @@ import Data.List (nub,isPrefixOf,findIndex)
 -- > import XMonad.Prompt.MPD
 -- > import qualified Network.MPD as MPD
 --
--- You can then use this in a keybinding, to filter first by artist, then by album and add 
--- the matching songs:
--- 
--- > addMatching MPD.withMPD defaultXPConfig [MPD.sgArtist, MPD.sgAlbum] >> return ()
+-- You can then use this in a keybinding, to filter first by artist, then by
+-- album and add the matching songs:
 --
--- If you need a password to connect to your MPD or need a different host/port, you can pass a
--- partially applied withMPDEx to the function:
+-- > addMatching MPD.withMPD defaultXPConfig [MPD.Artist, MPD.Album >> return ()
 --
--- > addMatching (MPD.withMPDEx "your.host" 666 (return $ Just $ "password")) ..
+-- That way you will first be asked for an artist name, then for an album by
+-- that artist etc..
+--
+-- If you need a password to connect to your MPD or need a different host/port,
+-- you can pass a partially applied withMPDEx to the function:
+--
+-- > addMatching (MPD.withMPDEx "your.host" 4242 "very secret") ..
 --
 
--- | Allows the user to supply a custom way to connect to MPD (e.g. partially applied withMPDEx).
+-- | Allows the user to supply a custom way to connect to MPD (e.g. partially
+-- applied withMPDEx).
 type RunMPD = forall a . MPD a -> IO (Response a)
+
+-- | Extracts the given metadata attribute from a Song
+extractMetadata :: Metadata -> Song -> String
+extractMetadata meta = fromMaybe "Unknown" . join . fmap listToMaybe .
+                       M.lookup meta . sgTags
 
 -- | Creates a case-insensitive completion function from a list.
 mkComplLst :: [String] -> String -> IO [String]
-mkComplLst lst s = return . filter (\s' -> map toLower s `isPrefixOf` map toLower s') $ lst
+mkComplLst lst s = return . filter isPrefix' $ lst
+    where isPrefix' s' = map toLower s `isPrefixOf` map toLower s'
 
-findMatching' :: XPConfig -> [Song] -> (Song -> String) -> X [Song]
-findMatching' xp s m = do
-  Just input <- inputPromptWithCompl xp "MPD" (mkComplLst . nub . map m $ s)
-  return $ filter ((==input) . m) s
+-- | Helper function for 'findMatching'
+findMatching' :: XPConfig -> [Song] -> Metadata -> X [Song]
+findMatching' xp songs meta = do
+  Just input <- inputPromptWithCompl xp "MPD"
+                (mkComplLst . nub . map (extractMetadata meta) $ songs)
+  return $ filter ((==input) . extractMetadata meta) songs
 
 -- | Lets the user filter out non-matching with a prompt by supplied criteria.
-findMatching :: RunMPD -> XPConfig -> [Song -> String] -> X [Song]
-findMatching runMPD xp ms = do
-  -- rs <- io (runMPD $ rights `fmap` listAllInfo "")
-  -- workaround due to a bug in libmpd(reported and fixed in darcs).
-  -- should be replaced by the line above when a new version is released.
-  -- (last version with this bug: 0.3.1)
-  rs <- io . runMPD . fmap concat $ mapM findArtist =<< listArtists
-  case rs of
-    Left e -> io (hPutStrLn stderr $ "MPD error: " ++ show e)  >> return []
-    Right s -> foldM (findMatching' xp) s ms
+findMatching :: RunMPD -> XPConfig -> [Metadata] -> X [Song]
+findMatching runMPD xp metas = do
+  resp <- io . runMPD . listAllInfo $ ""
+  case resp of
+    Left err -> trace ("XMonad.Prompt.MPD: MPD returned an error: " ++ show err)
+                >> return []
+    Right songs -> foldM (findMatching' xp) songs metas
 
 -- | Determine playlist position of the song and add it, if it isn't present.
-findOrAdd :: Song -> MPD PLIndex
-findOrAdd s = playlistInfo Nothing >>= \pl -> do
+findOrAdd :: Song -> MPD Int
+findOrAdd s = playlistInfo Nothing >>= \pl ->
   case findIndex ((== sgFilePath s) . sgFilePath) pl of
-    Just i -> return . Pos . fromIntegral $ i
-    Nothing -> ID <$> (addId . sgFilePath $ s)
+    Just i -> return . fromIntegral $ i
+    Nothing -> flip addId Nothing . sgFilePath $ s
 
--- | Add all selected songs to the playlist if they are not on it.
-addMatching :: RunMPD -> XPConfig -> [Song -> String] -> X [PLIndex]
-addMatching r xp ms = findMatching r xp ms >>= fmap (either (const []) id) . io . r . mapM findOrAdd
+-- | Add all selected songs to the playlist if they are not in it.
+addMatching :: RunMPD -> XPConfig -> [Metadata] -> X [Int]
+addMatching runMPD xp metas = do
+  matches <- findMatching runMPD xp metas
+  fmap (either (const []) id) . io . runMPD . mapM findOrAdd $ matches
 
 -- | Add matching songs and play the first one.
-addAndPlay :: RunMPD -> XPConfig -> [Song -> String] -> X ()
-addAndPlay r xp ms = addMatching r xp ms >>= io . r . play . Just . head >> return ()
+addAndPlay :: RunMPD -> XPConfig -> [Metadata] -> X ()
+addAndPlay runMPD xp ms = addMatching runMPD xp ms >>=
+                          io . runMPD . play . Just . head >> return ()
